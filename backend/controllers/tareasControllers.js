2f77a68b6c607a1036e0df547bf03b91a92e9d90
unknown <fasuarezd@gmail.com>
2023-08-15 19:28:55 -0600

    CRUD de tareas completo

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,7 +1,11 @@
 const asyncHandler = require('express-async-handler')
+const Tarea = require('../models/tareasModel')
 
 const getTareas = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Obtener Tareas' })
+
+    const tareas = await Tarea.find()
+
+    res.status(200).json(tareas)
 })
 
 const setTarea = asyncHandler(async (req, res) => {
@@ -9,15 +13,40 @@ const setTarea = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Por favor teclea una tarea')
     }
-    res.status(201).json({ message: 'Crear Tareas' })
+
+    const tarea = await Tarea.create({
+        texto: req.body.texto
+    })
+    res.status(201).json(tarea)
 })
 
 const updateTarea = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Modificar la tarea ${req.params.id}` })
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if (!tarea) {
+        res.status(404)
+        throw new Error('La tarea no fué encontrada')
+    }
+
+    const updatedTarea = await Tarea.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(updatedTarea)
 })
 
 const deleteTarea = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Borrar la tarea ${req.params.id}` })
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if (!tarea) {
+        res.status(404)
+        throw new Error('La tarea no fué encontrada')
+    }
+
+    //const deletedTarea = await Tarea.findByIdAndDelete(req.params.id)
+    tarea.deleteOne()
+
+    res.status(200).json({ id: tarea._id })
 })
 
 module.exports = {
